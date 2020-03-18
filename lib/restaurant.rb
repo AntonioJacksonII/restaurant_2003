24f39220692ba2962656edd40b8429cb181fd509
Antonio Jackson <antoniojackson@gmail.com>
2020-03-18 14:01:11 -0600

    Final commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,16 @@
+class Restaurant
+  attr_reader :opening_time, :name, :dishes
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+  end
+
+  def dishes
+    @dishes = []
+  end
+
+  def closing_time(time)
+    @closing_time = @opening_time + time
+  end
+end
